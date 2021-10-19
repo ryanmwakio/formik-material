6218787ba5c33a814a8192627bbd4ebc1b3620b2
ryan mwakio <ryanmwakio6@gmail.com>
2021-10-19 22:00:15 +0300

    theme configuration

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
 import { AddCircle } from "@mui/icons-material";
-import { Button, Container, Typography, ButtonGroup } from "@mui/material";
+import {
+  Button,
+  Container,
+  Typography,
+  ButtonGroup,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
 import { React } from "react";
-import { css } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#12EDFF",
+    },
+    secondary: {
+      main: "#ff1744",
+    },
+  },
+});
 
 function App() {
-  const buttonStyle = css({
-    backGround: "red",
-  });
-
-  const time = new Date();
-
-  const now = time.getFullYear();
-  console.log(now);
-
   return (
-    <Container>
-      <Button
-        variant="contained"
-        endIcon={<AddCircle />}
-        style={buttonStyle}
-        color="secondary"
-      >
-        Add
-      </Button>
-      <Button
-        variant="outlined"
-        startIcon={<AddCircle />}
-        color="secondary"
-        style={{ marginLeft: ".5rem", marginRight: ".5rem" }}
-      >
-        New button
-      </Button>
-      <ButtonGroup color="secondary">
-        <Button>1</Button>
-        <Button>2</Button>
-        <Button>3</Button>
-      </ButtonGroup>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Button
+          variant="contained"
+          endIcon={<AddCircle />}
+          color="secondary"
+          disableElevation
+        >
+          Add
+        </Button>
+        <Button
+          variant="outlined"
+          startIcon={<AddCircle />}
+          color="secondary"
+          style={{ marginLeft: ".5rem", marginRight: ".5rem" }}
+        >
+          New button
+        </Button>
+        <ButtonGroup color="secondary">
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+        </ButtonGroup>
 
-      <hr />
-      <br />
+        <hr />
+        <br />
 
-      <Typography variant="h4" color="textSecondary">
-        Boilerplate by Ryan
-      </Typography>
-    </Container>
+        <Typography variant="body2" color="textSecondary">
+          Material boilerplate &reg; Ryan
+        </Typography>
+      </Container>
+    </ThemeProvider>
   );
 }
 
